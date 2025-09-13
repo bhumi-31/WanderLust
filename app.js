e829950792836f6e7d188cedc1a3d8cdf424e77f
Bhumika Narula <146376815+bhumi-31@users.noreply.github.com>
2025-09-14 01:06:40 +0530

    Update env

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ const sessionOptions = {
         expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge : 7 * 24 * 60 * 60 * 1000,
         httpOnly : true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
         // samSite: "lax"
     }
 };
