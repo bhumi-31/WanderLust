c1c9b977b935109ee87e0b177dde025aa2e0e88d
Bhumika Narula <146376815+bhumi-31@users.noreply.github.com>
2025-09-15 12:13:06 +0530

    Update

--- controllers/users.js
+++ controllers/users.js
@@ -4,21 +4,21 @@ module.exports.renderSignupForm = (req, res) => {
     res.render("users/signup.ejs");
 };
 
-module.exports.signup = async(req, res) => {
+module.exports.signup = async(req, res, next) => {
     try{
         let {username, email, password} = req.body;
         const newUser = new User({email, username});
         const registeredUser = await User.register(newUser, password);
         req.login(registeredUser, (err) => {
             if(err){
-                next(err);
+                return next(err);
             }
             req.flash("success", "welcome to WanderLust!");
             res.redirect("/listings");
         });
     }catch(e) {
         req.flash("error", e.message);
-        res.redirect("/signup");
+        return res.redirect("/signup");
     }
 };
 
